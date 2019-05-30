c3af026e4e978b64a7d6e6f92a9955ccdb6535b8
Michael Milton <ttmigueltt@gmail.com>
2019-05-31 09:56:00 +1000

    File upload and parsing complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {Formik} from 'formik';
+import FileReaderStream from 'filereader-stream';
+import gff from '@gmod/gff';
 
 function App() {
     return (
@@ -23,24 +25,29 @@ function App() {
                     Upload Annotation
                 </Typography>
                 <Formik
-                    initialValues={{email: '', password: ''}}
+                    initialValues={{
+                        file: undefined,
+                        contig: '',
+                        coordsStart: 2,
+                        coordsEnd: 1
+                    }}
                     validate={values => {
+                        console.log(values);
                         let errors = {};
-                        if (!values.email) {
-                            errors.email = 'Required';
-                        }
-                        else if (
-                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                        ) {
-                            errors.email = 'Invalid email address';
-                        }
                         return errors;
                     }}
                     onSubmit={(values, {setSubmitting}) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
+                        console.log(values);
+                        FileReaderStream(values.file)
+                            .pipe(gff.parseStream())
+                            .on('data', data => {
+                                console.log('got item', data);
+                                return data;
+                            })
+                            .on('end', () => {
+                                console.log('done parsing!');
+                            });
+
                     }}
                 >
                     {({
@@ -51,9 +58,10 @@ function App() {
                           handleBlur,
                           handleSubmit,
                           isSubmitting,
+                          setFieldValue
                           /* and other goodies */
                       }) => (
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <FormControl margin="normal" required fullWidth>
                                 <input
                                     style={{
@@ -63,35 +71,51 @@ function App() {
                                     id="file"
                                     multiple
                                     type="file"
+                                    // value={values.file}
+                                    onBlur={handleBlur}
+                                    onChange={(event) => {
+                                        setFieldValue("file", event.currentTarget.files[0]);
+                                    }}
                                 />
+                                {errors.file && touched.file && errors.file}
                                 <label htmlFor="file">
-                                    <Button raised component="span">
-                                        Upload
+                                    <Button variant='outlined' component="span">
+                                        Upload GFF
                                     </Button>
                                 </label>
                             </FormControl>
                             <FormControl margin="normal" required fullWidth>
                                 <InputLabel htmlFor="contig">Operon Contig</InputLabel>
-                                <Input name="contig" type="text" id="contig"/>
+                                <Input name="contig" type="text" id="contig" value={values.contig}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}/>
+                                {errors.contig && touched.contig && errors.contig}
                             </FormControl>
                             <FormControl margin="normal" required fullWidth>
                                 <InputLabel htmlFor="coords">Position Start</InputLabel>
-                                <Input name="coordsStart" type="number" id="coordsStart"/>
+                                <Input name="coordsStart" type="number" id="coordsStart" value={values.coordsStart}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}/>
+                                {errors.coordsStart && touched.coordsStart && errors.coordsStart}
                             </FormControl>
                             <FormControl margin="normal" required fullWidth>
                                 <InputLabel htmlFor="coords">Position End</InputLabel>
-                                <Input name="coordsEnd" type="number" id="coordsEnd"/>
+                                <Input name="coordsEnd" type="number" id="coordsEnd" value={values.coordsEnd}
+                                       onChange={handleChange}
+                                       onBlur={handleBlur}/>
+                                {errors.coordsEnd && touched.coordsEnd && errors.coordsEnd}
                             </FormControl>
                             <Button
                                 type="submit"
                                 fullWidth
                                 variant="contained"
                                 color="primary"
+                                disabled={isSubmitting}
                             >
                                 Plot
                             </Button>
                         </form>
-                    ) }
+                    )}
                 </Formik>
             </Paper>
         </div>
