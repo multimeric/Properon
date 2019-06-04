eae5330e9a64a4f111361042db2e66a0240fb395
Michael Milton <ttmigueltt@gmail.com>
2019-06-04 18:37:04 +1000

    Working plots

--- src/App.js
+++ src/App.js
@@ -2,124 +2,192 @@ import React from 'react';
 import './App.css';
 import Input from '@material-ui/core/Input';
 import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import {Formik} from 'formik';
 import FileReaderStream from 'filereader-stream';
 import gff from '@gmod/gff';
+import * as ScriblCanvas from 'scribl';
 
-function App() {
-    return (
-        <div className="App">
-            <Paper style={{
-                width: '500px',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                flexDirection: 'column',
-                display: 'flex',
-                padding: '33px'
-            }}>
-                <Typography component="h1" variant="h5">
-                    Upload Annotation
-                </Typography>
-                <Formik
-                    initialValues={{
-                        file: undefined,
-                        contig: '',
-                        coordsStart: 2,
-                        coordsEnd: 1
-                    }}
-                    validate={values => {
-                        console.log(values);
-                        let errors = {};
-                        return errors;
-                    }}
-                    onSubmit={(values, {setSubmitting}) => {
-                        console.log(values);
-                        FileReaderStream(values.file)
-                            .pipe(gff.parseStream())
-                            .on('data', data => {
-                                console.log('got item', data);
-                                return data;
-                            })
-                            .on('end', () => {
-                                console.log('done parsing!');
-                            });
+import Scribl from './Scribl';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tracks: [],
+            width: 500
+        };
+    }
+    
+    static pickContig(){
+        
+    }
+
+    render() {
+        return (
+            <Grid
+                justify={'space-around'}
+                alignItems={'center'}
+                direction="column"
+                container
+                spacing={32}
+            >
+                <Grid
+                    style={{
+                        maxWidth: '600px'
                     }}
-                >
-                    {({
-                          values,
-                          errors,
-                          touched,
-                          handleChange,
-                          handleBlur,
-                          handleSubmit,
-                          isSubmitting,
-                          setFieldValue
-                          /* and other goodies */
-                      }) => (
-                        <form onSubmit={handleSubmit}>
-                            <FormControl margin="normal" required fullWidth>
-                                <input
-                                    style={{
-                                        display: 'none'
-                                    }}
-                                    accept="image/*"
-                                    id="file"
-                                    multiple
-                                    type="file"
-                                    // value={values.file}
-                                    onBlur={handleBlur}
-                                    onChange={(event) => {
-                                        setFieldValue("file", event.currentTarget.files[0]);
-                                    }}
-                                />
-                                {errors.file && touched.file && errors.file}
-                                <label htmlFor="file">
-                                    <Button variant='outlined' component="span">
-                                        Upload GFF
+                    item>
+
+                    {/*<Grid style={{*/}
+                    {/*    maxWidth: '500px'*/}
+                    {/*}} container spacing={'20px'} direction={'column'} justify={'center'} alignItems={'center'}*/}
+                    {/*>*/}
+                    <Card style={{
+                        padding: '33px'
+                    }}>
+                        <Typography component="h1" variant="h5">
+                            Upload Annotation
+                        </Typography>
+                        <Formik
+                            initialValues={{
+                                file: undefined,
+                                contig: '',
+                                coordsStart: 2,
+                                coordsEnd: 1
+                            }}
+                            validate={values => {
+                                let errors = {};
+                                if (!values.file)
+                                    errors.file = 'Required';
+                                if (!values.contig)
+                                    errors.contig = 'Required';
+                                if (!values.coordsStart)
+                                    errors.coordsStart = 'Required';
+                                if (!values.coordsEnd)
+                                    errors.coordsEnd = 'Required';
+                                if (values.coordsStart >= values.coordsEnd)
+                                    errors.coords = 'End coordinate must be higher than the start coordinate';
+                                return errors;
+                            }}
+                            onSubmit={(values, {setSubmitting}) => {
+                                const genes = [];
+                                FileReaderStream(values.file)
+                                    .pipe(gff.parseStream())
+                                    .on('data', features => {
+                                        for (const feature of features) {
+                                            if (feature.type === 'gene'
+                                                && feature.seq_id === values.contig
+                                                && feature.start >= values.coordsStart
+                                                && feature.end <= values.coordsEnd
+                                            ) {
+                                                genes.push(
+                                                    new ScriblCanvas.BlockArrow('gene', feature.start, feature.end - feature.start + 1, feature.strand)
+                                                );
+                                            }
+                                        }
+                                    })
+                                    .on('end', () => {
+                                        this.setState({
+                                            tracks: [genes]
+                                        });
+                                        setSubmitting(false);
+                                    });
+
+                            }}
+                        >
+                            {({
+                                  values,
+                                  errors,
+                                  touched,
+                                  handleChange,
+                                  handleBlur,
+                                  handleSubmit,
+                                  isSubmitting,
+                                  setFieldValue
+                                  /* and other goodies */
+                              }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <FormControl margin="normal" required fullWidth>
+                                        <input
+                                            style={{
+                                                display: 'none'
+                                            }}
+                                            accept="image/*"
+                                            id="file"
+                                            multiple
+                                            type="file"
+                                            // value={values.file}
+                                            onBlur={handleBlur}
+                                            onChange={(event) => {
+                                                setFieldValue('file', event.currentTarget.files[0]);
+                                            }}
+                                        />
+                                        {errors.file && touched.file && errors.file}
+                                        <label htmlFor="file">
+                                            <Button variant='outlined' component="span">
+                                                Upload GFF
+                                            </Button>
+                                        </label>
+                                    </FormControl>
+                                    <FormControl margin="normal" required fullWidth>
+                                        <InputLabel htmlFor="contig">Operon Contig</InputLabel>
+                                        <Input name="contig" type="text" id="contig" value={values.contig}
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}/>
+                                        {errors.contig && touched.contig && errors.contig}
+                                    </FormControl>
+                                    <FormControl margin="normal" required fullWidth>
+                                        <InputLabel htmlFor="coords">Position Start</InputLabel>
+                                        <Input name="coordsStart" type="number" id="coordsStart"
+                                               value={values.coordsStart}
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}/>
+                                        {errors.coordsStart && touched.coordsStart && errors.coordsStart}
+                                    </FormControl>
+                                    <FormControl margin="normal" required fullWidth>
+                                        <InputLabel htmlFor="coords">Position End</InputLabel>
+                                        <Input name="coordsEnd" type="number" id="coordsEnd"
+                                               value={values.coordsEnd}
+                                               onChange={handleChange}
+                                               onBlur={handleBlur}/>
+                                        {errors.coordsEnd && touched.coordsEnd && errors.coordsEnd}
+                                    </FormControl>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                    >
+                                        Plot
                                     </Button>
-                                </label>
-                            </FormControl>
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="contig">Operon Contig</InputLabel>
-                                <Input name="contig" type="text" id="contig" value={values.contig}
-                                       onChange={handleChange}
-                                       onBlur={handleBlur}/>
-                                {errors.contig && touched.contig && errors.contig}
-                            </FormControl>
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="coords">Position Start</InputLabel>
-                                <Input name="coordsStart" type="number" id="coordsStart" value={values.coordsStart}
-                                       onChange={handleChange}
-                                       onBlur={handleBlur}/>
-                                {errors.coordsStart && touched.coordsStart && errors.coordsStart}
-                            </FormControl>
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="coords">Position End</InputLabel>
-                                <Input name="coordsEnd" type="number" id="coordsEnd" value={values.coordsEnd}
-                                       onChange={handleChange}
-                                       onBlur={handleBlur}/>
-                                {errors.coordsEnd && touched.coordsEnd && errors.coordsEnd}
-                            </FormControl>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                disabled={isSubmitting}
-                            >
-                                Plot
-                            </Button>
-                        </form>
-                    )}
-                </Formik>
-            </Paper>
-        </div>
-    );
+                                </form>
+                            )}
+                        </Formik>
+                    </Card>
+                </Grid>
+                <Grid
+                    style={{
+                        maxWidth: '600px'
+                    }}
+                    item>
+                    <Card style={{
+                        padding: '33px'
+                    }}>
+                        <Scribl tracks={this.state.tracks} width={this.state.width}/>
+                    </Card>
+                </Grid>
+
+                {/*</Grid>*/}
+                {/*</Grid>*/}
+            </Grid>
+        );
+    }
 }
 
 export default App;
---
+++ src/Scribl.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import * as ScriblCanvas from 'scribl';
+
+export default function Scribl(props)  {
+    const {tracks, width, ...rest} = props;
+    const canvasRef = useRef(null);
+    
+    const layout = (
+        <canvas width={width} ref={canvasRef} {...rest}/>
+    );
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        const scribl = new ScriblCanvas.Scribl(canvas, width);
+        for (const glyphs of tracks){
+            const track = scribl.addTrack();
+            for (const glyph of glyphs){
+                track.addFeature(glyph);
+            }
+        }
+        scribl.draw();
+    });
+    
+    return layout;
+}
+
+Scribl.propTypes = {
+    /**
+     * An array of tracks. Each track is an array of glyphs to render on that track
+     */
+    tracks: PropTypes.arrayOf(PropTypes.instanceOf(Scribl.Glyph)),
+
+    /**
+     * Width of the diagram
+     */
+    width: PropTypes.number
+};
